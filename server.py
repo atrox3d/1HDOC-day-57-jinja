f93740e4f70692998d5d67485318dd24acec2213
atrox3d <atrox3d@gmail.com>
2021-03-22 14:50:49 +0100

    add variables into template

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 import util.network
+import random
+from datetime import datetime as dt
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    random_number = random.randint(1, 10)
+    year = dt.now().year
+    return render_template("index.html", num=random_number, year=year)
 
 
 if __name__ == "__main__":
